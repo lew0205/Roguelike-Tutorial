ecf6f9e836b4473ccb72529be7bfa0b552a92e51
lew0205 <lew0205@naver.com>
2021-12-08 14:26:53 +0900

    21/12/08 적 체력바 구현

--- Assets/Scripts/EnemyDamaged.cs
+++ Assets/Scripts/EnemyDamaged.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyDamaged : MonoBehaviour
 {
     public float health; // 체력
     public float maxHealth; // 최대 체력
+
+    public GameObject healthBar;
+    public Slider healthBarSlider;
 
     void Start()
     {
@@ -14,8 +18,17 @@ public class EnemyDamaged : MonoBehaviour
 
     public void DealDamage(float damage)
     {
+        healthBar.SetActive(true);
+        healthBarSlider.value = CalculateHealthPercentage();
         health -= damage; // 체력에서 데미지만큼 감소
         CheckDeath(); // 죽었는지 확인
+    }
+
+    public void HealCharacter(float heal)
+    {
+        health += heal; // 체력에서 회복량만큼 추가
+        CheckOverheal(); // 회복된 체력이 최대체력보다 큰지 확인
+        healthBarSlider.value = CalculateHealthPercentage();
     }
 
     private void CheckOverheal()
@@ -32,5 +45,10 @@ public class EnemyDamaged : MonoBehaviour
         {
             Destroy(gameObject); // 적 파괴
         }
+    }
+
+    private float CalculateHealthPercentage()
+    {
+        return (health / maxHealth);
     }
 }
